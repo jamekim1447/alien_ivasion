07a96ac536c8b580ca978be142666f25594301e4
jamekim1447 <kimjame13@gmail.com>
2020-04-10 21:15:32 +0430

    setting plus updates

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,20 @@
 import sys
-
 import pygame
+from .setting import Settings
 
 class AlienInvasion:
     "starting the main game"
     pygame.get_init()
 
     def __init__(self):
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings
+
+        self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+
+
+
 
     def run_game(self):
         "this runs the game and updates display"
@@ -16,6 +22,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #accessing to the corect function for color background
+            self.screen.fill(self.settings.screen_color)
 
             pygame.display.flip()
 if __name__=="__main__":
---
+++ setting.py
@@ -0,0 +1,8 @@
+class Settings():
+    def __init__(self):
+        "this will be our settings info"
+        self.screen_width=1000
+        self.screen_height=600
+        self.screen_color=(230,230,230)
+        
+
